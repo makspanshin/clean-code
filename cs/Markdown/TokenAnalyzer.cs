c43679090c765120fb1a3a4305e0a8209892718b
makspanshin <makspanshin97@gmail.com>
2021-11-23 14:55:34 +0500

    Create TokenAnalyzer.cs

---
+++ cs/Markdown/TokenAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Markdown.Interfaces;
+using Markdown.Model;
+
+namespace Markdown
+{
+    internal class TokenAnalyzer
+    {
+        public void FindOpenOrCloseBoldToken(List<IToken> tokens)
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                BoldToken tmp = new BoldToken();
+                if (tokens[i].GetType() == typeof(BoldToken))
+                {
+
+                }
+            }
+        }
+
+        public void FindOpenOrCloseItalicToken(List<IToken> tokens)
+        {
+
+        }
+
+        public void FindOpenOrCloseHeaderToken(List<IToken> tokens)
+        {
+
+        }
+    }
+}
